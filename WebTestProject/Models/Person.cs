43cb39bce38ec23fe7352dc103338603b045f31c
luciusgustavo <31082755+luciusgustavo@users.noreply.github.com>
2017-08-16 20:25:19 -0300

    Update Person.cs
    Add Telephone in person

--- WebTestProject/Models/Person.cs
+++ WebTestProject/Models/Person.cs
@@ -7,6 +7,7 @@ namespace WebTestProject.Models
 {
     public class Person
     {
+        public string Telephone { get; set; }
         public string Cpf { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
